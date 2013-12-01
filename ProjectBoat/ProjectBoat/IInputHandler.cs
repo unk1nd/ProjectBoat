463038016cdb90d12a8c51d7c49c84088044e022
Mikael Bendiksen <duuxman@gmail.com>
2013-12-01 15:38:30 +0100

    planning feechers

---
+++ ProjectBoat/ProjectBoat/IInputHandler.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace ProjectBoat
+{
+    public interface IInputHandler
+    {
+        KeyboardState KeyboardState { get; }
+    };
+
+    /// <summary>
+    /// This is a game component that implements IUpdateable.
+    /// </summary>
+    public class InputHandler : Microsoft.Xna.Framework.GameComponent, IInputHandler
+    {
+        private KeyboardState keyboardState;
+        public KeyboardState KeyboardState
+        {
+            get { return (keyboardState); }
+        }
+
+
+        public InputHandler(Game game)
+            : base(game)
+        {
+            // TODO: Construct any child components here
+            game.Services.AddService(typeof(IInputHandler), this);
+        }
+
+        /// <summary>
+        /// Allows the game component to perform any initialization it needs to before starting
+        /// to run.  This is where it can query for any required services and load content.
+        /// </summary>
+        public override void Initialize()
+        {
+            // TODO: Add your initialization code here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// Allows the game component to update itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update code here
+
+            keyboardState = Keyboard.GetState();
+
+            if (keyboardState.IsKeyDown(Keys.Escape))
+            {
+                Game.Exit();
+            }
+            base.Update(gameTime);
+        }
+    }
+}
--- ProjectBoat/ProjectBoat/boat.cs
+++ ProjectBoat/ProjectBoat/boat.cs
@@ -2,10 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 
 namespace ProjectBoat
 {
     class boat
     {
+        private double x;
+        private double y;
+        private float boatRotY;
+        private Vector3 boatPosition;
+        private Model planetModel;
+        private Texture2D boatTexture;
+
+        public Texture2D BoatTexture
+        {
+            get { return boatTexture; }
+            set { boatTexture = value; }
+        }
+
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public float BoatRotY
+        {
+            get { return boatRotY; }
+            set { boatRotY = value; }
+        }
+
+        public Vector3 BoatPosition 
+        {
+            get { return boatPosition; }
+            set { boatPosition = value; }
+        }
     }
 }
